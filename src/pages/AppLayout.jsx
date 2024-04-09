45828682b0a0e4cb03e367d9302fbf27f020fce4
Warity <vitaliko1998@gmail.com>
2024-04-09 09:55:38 +0300

    fixing synt error

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Map from "../components/Map";
-import Sidebar from "../components/SideBar";
+import Sidebar from "../components/Sidebar";
 import User from "../components/User";
 import styles from "./AppLayout.module.css";
 
